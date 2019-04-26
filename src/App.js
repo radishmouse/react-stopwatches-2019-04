c78e12ace206a73785c639c603f93b898ab79f0f
chris aquino <chris@radishmouse.com>
2019-04-26 11:31:03 -0400

    increments the timer once a second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ElapsedTime from './ElapsedTime';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    }
+    this.clock = 0;
+  }
+
+  componentDidMount() {
+    console.log('starting clock');
+    requestAnimationFrame(this._tick);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+       <ElapsedTime time={this.state.time} />
+      </div>
+    );
+  }
+
+  _tick = () => {
+    // console.log('tick')
+    // intentionally using an instance variable, not state
+    // we'll use the variable to determine when to increase this.state.time
+    this.clock++;
+
+    if (this.clock % 60 === 0) {
+      console.log('clock tick');
+      this.setState({
+        time: this.state.time + 1
+      })
+    }
+
+    // This causes the browser to call our _tick
+    // method every 60th of a second (roughly)
+    requestAnimationFrame(this._tick);
+  }
 }
-
 export default App;
---
+++ src/ElapsedTime.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ElapsedTime({time}) {
+    return (
+        <div>
+            {time}
+        </div>
+    );
+}
+
+export default ElapsedTime;
