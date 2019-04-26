7f632128e8b74e2790e0782116c5d2e590232670
chris aquino <chris@radishmouse.com>
2019-04-26 12:37:20 -0400

    slightly better version of pausing

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -16,16 +16,31 @@ class Stopwatch extends React.Component {
 
   componentDidMount() {
     console.log('starting clock');
-    setInterval(() => {
-        if (!this.state.isRunning) {
-            this.setState({
-                pausedFor: this.state.pausedFor + 1
-            })
-        }
-    }, 1000);
+    // Buggy way to set the pause value.
+    // Why? because it gets out of sync with the global clock.
+    // setInterval(() => {
+    //     if (!this.state.isRunning) {
+    //         this.setState({
+    //             pausedFor: this.state.pausedFor + 1
+    //         })
+    //     }
+    // }, 1000);
+  }
+
+  // This method lets us manipulate state right before .render() is called.
+  // It is called when new props are passed to the component.
+  // We don't necessarily need to use any of the values from prop - we know that we get passed
+  // new props each tick of the global clock.
+  // If this Stopwatch is paused, when it receive a clock tick, we need to increment value of pausedFor.
+  static getDerivedStateFromProps(props, state) {
+    return {
+        pausedFor: state.isRunning ? state.pausedFor : state.pausedFor + 1
+    }
   }
   
   render() {
+    // We calculate the new clock value based on the new clock value passed to us, minus when we started,
+    // minus how long we're paused.
     let clockValue = this.props.clock - this.state.originalClock - this.state.pausedFor;
     return (
       <div className="Stopwatch">
@@ -38,7 +53,8 @@ class Stopwatch extends React.Component {
 
   _toggleIsRunning = () => {
     this.setState({
-      isRunning: !this.state.isRunning  // Use the ! operator to flip the boolean
+      isRunning: !this.state.isRunning,  // Use the ! operator to flip the boolean
+      pausedFor: this.state.pausedFor - 1  // Weird way to make sure it doesn't subtract an extra second on paus.
     });
   }
 
